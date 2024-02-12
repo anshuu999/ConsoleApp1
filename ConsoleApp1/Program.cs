8de2dfee02bbcc2c28528fa87f006b68fc2970c1
v119847_ual <riddhivardhan.dubey@united.com>
2024-02-12 12:08:33 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = ConvertJson();
+            Console.WriteLine(json);
+            ConvertJsonToObject(json);
+            Console.ReadLine();
+
+        }
+        static string ConvertJson()
+        {
+            Emp emp = new Emp() { Id = 1, Name = "John", Salary = 10000.ToString() };
+
+
+
+            string json = JsonConvert.SerializeObject(emp);
+            return json;
+        }
+        static void ConvertJsonToObject(string json)
+        {
+            Emp m = JsonConvert.DeserializeObject<Emp>(json);
+            Console.WriteLine(m.Id);
+            Console.WriteLine(m.Name);
+            Console.WriteLine(m.Salary);
+        }
+    }
+
+    class Emp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+        public Emp() { }
+
+
+
+    }
+}
